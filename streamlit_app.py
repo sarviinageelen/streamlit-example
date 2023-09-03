ae6c852ca6c61bbf7e38b5cb8cf1ed412bb4345a
Sarviin Ageelen <sarviinageelen@users.noreply.github.com>
2023-09-03 11:27:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def fetch_luno_minute_data():
     # Convert timestamp to datetime format
     df = pd.DataFrame({
         'time': pd.to_datetime(data['t'], unit='s'),
-        'close': data['c']
+        'close_XBTMYR': data['c']
     })
     
     # Adjust the time to GMT+8
@@ -23,10 +23,29 @@ def fetch_luno_minute_data():
     
     return df
 
-st.title('Luno XBTMYR Data (Close Price)')
+def fetch_binance_minute_data():
+    url = "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=500"
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        st.error("Failed to fetch data from Binance.")
+        return None
+    
+    data = response.json()
+    
+    df = pd.DataFrame(data, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+    
+    # Convert timestamp to GMT+8 date-time format
+    df['open_time'] = pd.to_datetime(df['open_time'], unit='ms') + pd.Timedelta(hours=8)
+    
+    return df[['open_time', 'close']].rename(columns={'open_time': 'time', 'close': 'close_BTCUSDT'})
 
-df = fetch_luno_minute_data()
+st.title('Luno XBTMYR and Binance BTCUSDT Data (Close Price)')
 
-if df is not None:
-    st.write(df)
-    st.line_chart(df.set_index('time'))
+luno_df = fetch_luno_minute_data()
+binance_df = fetch_binance_minute_data()
+
+if luno_df is not None and binance_df is not None:
+    merged_df = pd.merge_asof(luno_df, binance_df, on='time', direction='nearest').set_index('time')
+    st.write(merged_df)
+    st.line_chart(merged_df)
